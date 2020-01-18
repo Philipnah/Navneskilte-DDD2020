a08c46bc2e7819aa4a45363eef173df689fd65d6
Philipnah <Philip@han.dk>
2020-01-19 00:11:29 +0100

    added new function

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-
-
 # We create the letters structure here in the order M, I, R, K, O
-line1 = ["## ##", "#####", "####", "#   #", " ### "]
+line1 = ["## ##", "#####", "#### ", "#   #", " ### "]
 line2 = ["# # #", "  #  ", "#   #", "#  # ", "#   #"]
 line3 = ["# # #", "  #  ", "#### ", "###  ", "#   #"]
 line4 = ["#   #", "  #  ", "#   #", "#  # ", "#   #"]
@@ -15,26 +13,64 @@ def mainfunc():
         print(line1[i] + " ", end="")
         i += 1
 
+    print("")
     i = 0
     while i <= 4:
-        print("\n" + line2[i] + " ", end="")
+        print(line2[i] + " ", end="")
         i += 1
 
+    print("")
     i = 0
     while i <= 4:
-        print("\n" + line3[i] + " ", end="")
+        print(line3[i] + " ", end="")
         i += 1
 
+    print("")
     i = 0
     while i <= 4:
-        print("\n" + line4[i] + " ", end="")
+        print(line4[i] + " ", end="")
         i += 1
 
+    print("")
     i = 0
     while i <= 4:
-        print("\n" + line5[i] + " ", end="")
+        print(line5[i] + " ", end="")
         i += 1
+
+def printLetter():
+    i = 0
+    letters = input()
+    
+    individualLetters = letters.split()
+    print(individualLetters[0])
+
+    if individualLetters[0] == "m" or "M":
+        i = 0
+
+    if individualLetters[0] == "i" or "I":
+        i = 1
+
+    if individualLetters[0] == "r" or "R":
+        i = 2
+
+    if individualLetters[0] == "k" or "K":
+        i = 3
+
+    if individualLetters[0] == "o" or "O":
+        i = 4
+    
+    else:
+        print("You must use only the letters m, i, r, k, o!")
+
+    print(i)
+    print(line1[i])
+    print(line2[i])
+    print(line3[i])
+    print(line4[i])
+    print(line5[i])
+
 
 
 if __name__ == "__main__":
-    mainfunc()
+    # mainfunc()
+    printLetter()
