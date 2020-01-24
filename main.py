ac3e9694f190d171e564a22879d4946441e830fb
Philipnah <Philip@han.dk>
2020-01-24 08:45:46 +0100

    updates to main

--- main.py
+++ main.py
@@ -1,76 +1,71 @@
 # We create the letters structure here in the order M, I, R, K, O
-line1 = ["## ##", "#####", "#### ", "#   #", " ### "]
-line2 = ["# # #", "  #  ", "#   #", "#  # ", "#   #"]
-line3 = ["# # #", "  #  ", "#### ", "###  ", "#   #"]
-line4 = ["#   #", "  #  ", "#   #", "#  # ", "#   #"]
-line5 = ["#   #", "#####", "#   #", "#   #", " ### "]
+# line1 = ["## ##", "#####", "#### ", "#   #", " ### "]
+# line2 = ["# # #", "  #  ", "#   #", "#  # ", "#   #"]
+# line3 = ["# # #", "  #  ", "#### ", "###  ", "#   #"]
+# line4 = ["#   #", "  #  ", "#   #", "#  # ", "#   #"]
+# line5 = ["#   #", "#####", "#   #", "#   #", " ### "]
 
-# We create the main function here
 
-def mainfunc():
-    i = 0
-    while i <= 4:
-        print(line1[i] + " ", end="")
-        i += 1
-
-    print("")
-    i = 0
-    while i <= 4:
-        print(line2[i] + " ", end="")
-        i += 1
-
-    print("")
-    i = 0
-    while i <= 4:
-        print(line3[i] + " ", end="")
-        i += 1
-
-    print("")
-    i = 0
-    while i <= 4:
-        print(line4[i] + " ", end="")
-        i += 1
-
-    print("")
-    i = 0
-    while i <= 4:
-        print(line5[i] + " ", end="")
-        i += 1
-
-def printLetter():
-    i = 0
-    letters = input()
+letters = input()
     
-    individualLetters = letters.split()
-    print(individualLetters[0])
+individualLetters = letters.split()
 
-    if individualLetters[0] == "m" or "M":
+print(len(individualLetters))
+
+M = ["## ##", "# # #", "# # #", "#   #", "#   #"]
+I = ["#####", "  #  ", "  #  ", "  #  ", "#####"]
+R = ["#### ", "#   #", "#### ", "#   #", "#   #"]
+K = ["#   #", "#  # ", "###  ", "#  # ", "#   #"]
+O = [" ### ", "#   #", "#   #", "#   #", " ### "]
+
+def mainfunc(n):
+    if individualLetters[n] == "m":
         i = 0
-
-    if individualLetters[0] == "i" or "I":
-        i = 1
-
-    if individualLetters[0] == "r" or "R":
-        i = 2
-
-    if individualLetters[0] == "k" or "K":
-        i = 3
-
-    if individualLetters[0] == "o" or "O":
-        i = 4
-    
+        while i <= 4:
+            print(M[i] + " ")
+            i += 1
+            
+    if individualLetters[n] == "i":
+        i = 0
+        while i <= 4:
+            print(I[i] + " ")
+            i += 1
     else:
         print("You must use only the letters m, i, r, k, o!")
+    
 
-    print(i)
-    print(line1[i])
-    print(line2[i])
-    print(line3[i])
-    print(line4[i])
-    print(line5[i])
+# def printLetter(letter):
+#     i = 5
+
+#     if individualLetters[letter] == "m":
+#         print()
+#         i = 0
+
+#     if individualLetters[letter] == "i":
+#         i = 1
+
+#     if individualLetters[letter] == "r":
+#         i = 2
+
+#     if individualLetters[letter] == "k":
+#         i = 3
+
+#     if individualLetters[letter] == "o":
+#         i = 4
+    
+    
+
+#     print(i)
+#     print(line1[i])
+#     print(line2[i])
+#     print(line3[i])
+#     print(line4[i])
+#     print(line5[i])
 
 
 
 if __name__ == "__main__":
-    # mainfunc()
-    printLetter()
+    n = 0
+    while n <= len(individualLetters):
+        mainfunc(n)
+        n += 1
